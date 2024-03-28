6dbe229bd6783be39d8694e3d0c046c38f8ad600
Carmelo Gulino <s298109@studenti.polito.it>
2024-03-28 10:28:37 +0100

    indovina numero

--- controller.py
+++ controller.py
@@ -7,3 +7,65 @@ class Controller(object):
     def __init__(self, view: View):
         self._view = view
         self._model = Model()
+
+    def handleNuova(self, e):
+        """
+        inizio una nuova partita
+        """
+        self._view._txtMrim.value = self.getMmax()      #all'inizio rim=max
+        self._view._btnProva.disabled = False       #abilito il tasto prova
+        self._view._txtTentativo.disabled = False
+        self._view._lvOut.controls.clear()      #pulisce tutta la lvOut
+        self._view._lvOut.controls.append(ft.Text("Indovina il numero", color="green"))
+        self._model.inizializza()
+        self._view.update()
+
+    def handleProva(self, e):
+        """
+        controllo che il tentativo sia un intero, in caso negativo faccio comparire il messaggio e finisco la prova
+        """
+        tent = self._view._txtTentativo.value
+        try:
+            intTent = int(tent)
+        except ValueError:
+            self._view._lvOut.controls.append(ft.Text("Il tentativo deve essere un intero", color="red"))
+            self._view._txtTentativo.value = ""
+            self._view.update()
+            return
+
+        mRim, result = self._model.indovina(intTent)
+        self._view._txtMrim.value=mRim
+        self._view.update()
+
+        if mRim==0:     #se ho zero tentativi, ho perso ed eventualmente esco
+            self._view._btnProva.disabled=True      #ho vinto e disabilito la prova
+            self._view._txtTentativo.disabled=True      #ho vinto e disabilito la prova
+            self._view._lvOut.controls.append(ft.Text(f"Hai perso! Il segreto era: {self._model.segreto}"))
+            self._view._txtTentativo.value = ""
+            self._view.update()
+            return
+        if result==0:
+            self._view._lvOut.controls.append(ft.Text(f"Hai vinto!"))
+            self._view._btnProva.disabled=True      #ho vinto e disabilito la prova
+            self._view._txtTentativo.value = ""
+            self._view.update()
+            return
+        elif result == -1:
+            self._view._lvOut.controls.append(ft.Text(f"Il segreto è più piccolo"))
+            self._view._txtTentativo.value = ""
+            self._view.update()
+            return
+        else:
+            self._view._lvOut.controls.append(ft.Text(f"Il segreto è più grande"))
+            self._view._txtTentativo.value = ""
+            self._view.update()
+            return
+
+    def getNmax(self):
+        return self._model.NMax
+
+    def getMmax(self):
+        return self._model.Mmax
+
+    def getMrim(self):
+        return self._model.Mrim
--- model.py
+++ model.py
@@ -3,4 +3,42 @@ import random
 
 class Model(object):
     def __init__(self):
-        pass
+        self._NMax = 100
+        self._Mmax = 6
+        self._Mrim = None
+        self._segreto = None
+
+    @property       #dal momento che nMax è privato devo mettere property
+    def NMax(self):
+        return self._NMax
+
+    @property  # dal momento che nMax è privato devo mettere property
+    def Mmax(self):
+        return self._Mmax
+
+    @property  # dal momento che nMax è privato devo mettere property
+    def Mrim(self):
+        return self._Mrim
+
+    @property
+    def segreto(self):
+        return self._segreto
+
+    def inizializza(self):
+        self._segreto = random.randint(1, self._NMax)
+        self._Mrim = self._Mmax
+
+    def indovina(self, tentativo):
+
+        if self._Mrim == 0:
+            return self._Mrim, None
+        else:
+            self._Mrim -= 1
+
+        if tentativo == self._segreto:
+            return self._Mrim, 0
+        elif tentativo > self._segreto:
+            return self._Mrim, -1
+        else:
+            return self._Mrim, 1
+
